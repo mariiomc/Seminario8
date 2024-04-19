80f85a24404847faa91957319d616da05736d7a2
intxz <angel.redondo@estudiantat.upc.edu>
2024-04-19 12:15:12 +0200

    validations

--- src/POST/createUser.tsx
+++ src/POST/createUser.tsx
@@ -1,82 +1,136 @@
- import { useState, useEffect } from "react";
- import axios from "axios";
- import { User } from "../modules/user";
-import { strict } from "assert";
+import { useState } from "react";
+import axios from "axios";
+import { User } from "../modules/user";
 
 interface CreateUserProps {
     updateUserList: () => void;
 }
 
-function CreateUser({ updateUserList } : CreateUserProps) {
-    const [first_name, setFirsName] = useState('');
+interface FormErrors {
+    [key: string]: string;
+}
+
+function CreateUser({ updateUserList }: CreateUserProps) {
+    const [first_name, setFirstName] = useState('');
     const [middle_name, setMiddleName] = useState('');
     const [last_name, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [phone_number, setPhoneNumber] = useState('');
     const [gender, setGender] = useState('');
-    
-    const Submit = (e: any) => {
+    const [errors, setErrors] = useState<FormErrors>({});
+
+    const validateField = (fieldName: string, value: string) => {
+        let errorMessage = '';
+
+        switch (fieldName) {
+            case 'first_name':
+                errorMessage = value.trim() === '' ? 'First name is required' : '';
+                break;
+            case 'middle_name':
+                errorMessage = value.trim() === '' ? 'Middle name is required' : '';
+                break;
+            case 'last_name':
+                errorMessage = value.trim() === '' ? 'Last name is required' : '';
+                break;
+            case 'email':
+                errorMessage = value.trim() === '' ? 'Email is required' : !isValidEmail(value) ? 'Invalid email format' : '';
+                break;
+            default:
+                break;
+        }
+
+        setErrors(prevErrors => ({
+            ...prevErrors,
+            [fieldName]: errorMessage
+        }));
+    };
+
+    const isValidEmail = (email: string) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const user : User = {
-            name: {
-                first_name: first_name ,
-                middle_name: middle_name,
-                last_name: last_name,
-            },
-            email: email,
-            phone_number: phone_number,
-            gender: gender
-    
-        };
+        const isFormValid = validateForm();
+        if (isFormValid) {
+            const user: User = {
+                name: {
+                    first_name: first_name,
+                    middle_name: middle_name,
+                    last_name: last_name,
+                },
+                email: email,
+                phone_number: phone_number,
+                gender: gender
+            };
+            axios.post("http://localhost:3000/user", user)
+                .then(result => {
+                    console.log(result);
+                    updateUserList();
+                    setFirstName('');
+                    setMiddleName('');
+                    setLastName('');
+                    setEmail('');
+                    setPhoneNumber('');
+                    setGender('');
+                })
+                .catch(err => console.error(err));
+        }
+    };
 
-        axios.post("http://localhost:3000/user", user)
-        .then(result => {
-            console.log(result)
-            updateUserList();
+    const validateForm = () => {
+        let isValid = true;
 
-            setFirsName('');
-            setMiddleName('');
-            setLastName('');
-            setEmail('');
-            setPhoneNumber('');
-            setGender('');
+        validateField('first_name', first_name);
+        validateField('middle_name', middle_name);
+        validateField('last_name', last_name);
+        validateField('email', email);
 
-        })
-        .catch(err => console.log(err));
-        console.log(user);
-    }
-        
+        for (const error in errors) {
+            if (errors[error] !== '') {
+                isValid = false;
+                break;
+            }
+        }
+
+        return isValid;
+    };
+
     return (
         <div>
-            <form onSubmit={Submit}>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>First Name</label>
-                    <input type="text" value={first_name} onChange={(e: any) => setFirsName(e.target.value)}/> 
+                    <input type="text" value={first_name} onChange={(e) => { setFirstName(e.target.value); validateField('first_name', e.target.value); }} />
+                    {errors.first_name && <span style={{ color: 'red' }}>{errors.first_name}</span>}
                 </div>
                 <div>
                     <label>Middle Name</label>
-                    <input type="text" value={middle_name} onChange={(e: any) => setMiddleName(e.target.value)}/> 
+                    <input type="text" value={middle_name} onChange={(e) => { setMiddleName(e.target.value); validateField('middle_name', e.target.value);}} />
+                    {errors.middle_name && <span style={{ color: 'red' }}>{errors.middle_name}</span>}
                 </div>
                 <div>
                     <label>Last Name</label>
-                    <input type="text" value={last_name} onChange={(e: any) => setLastName(e.target.value)}/> 
+                    <input type="text" value={last_name} onChange={(e) => { setLastName(e.target.value); validateField('last_name', e.target.value); }} />
+                    {errors.last_name && <span style={{ color: 'red' }}>{errors.last_name}</span>}
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="text" value={email} onChange={(e: any) => setEmail(e.target.value)}/> 
+                    <input type="text" value={email} onChange={(e) => { setEmail(e.target.value); validateField('email', e.target.value); }} />
+                    {errors.email && <span style={{ color: 'red' }}>{errors.email}</span>}
                 </div>
                 <div>
                     <label>Phone Number</label>
-                    <input type="text" value={phone_number} onChange={(e: any) => setPhoneNumber(e.target.value)}/> 
+                    <input type="text" value={phone_number} onChange={(e) => setPhoneNumber(e.target.value)} />
                 </div>
                 <div>
                     <label>Gender</label>
-                    <input type="text" value={gender} onChange={(e: any) => setGender(e.target.value)}/> 
+                    <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
                 </div>
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
-    )
+    );
 }
 
 export default CreateUser;
